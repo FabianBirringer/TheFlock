15f95bb375040306b6a18bf41ceaddfcbe2a34dc
Fabian Birringer <FabianBirringer@outlook.de>
2020-06-09 11:50:29 +0200

    Nachbar erkennung mittels Collision-Box implementiert
    mittels einer zusätzlichen Kollisions Box lassen sich Nachbarn im Radius der Kollisions box mittels der OnTriggerEnter und OnTriggerExit Mehtode in einem Array des Boids speichern bzw wieder rauswerfen.

---
+++ TheFlock/Assets/Scripts/BoidBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Globalization;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class getNeighbour : MonoBehaviour
+{
+    private ArrayList boids = new ArrayList();
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        GameObject go = Other.gameObject;
+        if (go.tag == "boid"&& go.GetInstanceID()!=GetInstanceID() && go.tag!="collider")
+        {
+            if (boids.Contains(go))
+            {
+                return;
+            }
+            boids.Add(go);
+        }
+    }
+    private void OnTriggerExit(Collider Other)
+    {
+        GameObject go = Other.gameObject;
+        if (go.tag == "boid" && go.GetInstanceID()!=GetInstanceID())
+        {
+            boids.Remove(go);
+        }
+    }
+}
